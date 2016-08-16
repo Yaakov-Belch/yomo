c51312f392800d3d372372b94708f07dad617f80
Yaakov Belch <github@yaakovnet.net>
2016-08-16 15:33:24 +0300

    ok

--- src/experimental.js
+++ src/experimental.js
@@ -3,7 +3,6 @@ import {cacheAsync,cacheSlow,auditor,runner,yomoPeek}
 import {reuse,combineReducers} from './util/reuse.js';
 import {newBridge,remoteFn,linkPipes,pipes,getPipe}
   from './core/ipc.js';
-import {insecureHttps} from './util/insecure-https.js';
 import {getBuffer,getText,getJSON} from './util/getx.js';
 import {delay,ticks} from './util/time.js';
 import mobx from 'mobx';
@@ -14,7 +13,6 @@ export {
   yomoPeek,               // remove API
   remoteFn,newBridge,getPipe,pipes,linkPipes, // refactor API
   reuse,combineReducers,  // release as separate package
-  insecureHttps,          // release as separate package
   getBuffer,getText,getJSON, // release as separate package
   delay,ticks,            // release as separate package
   mobx,                   // not needed --- already separate pkg
