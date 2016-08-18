144d299419b7a87982bb689457d4642d0d482929
Yaakov Belch <github@yaakovnet.net>
2016-08-18 22:38:31 +0300

    ok

--- src/core/mqtt-ipc.js
+++ src/core/mqtt-ipc.js
@@ -1,16 +1,20 @@
-import mqtt from 'mqtt';
-import {wr2,rd2,del2} from '../util/hash2.js';
-import {afterPrefix} from '../util/prefix.js';
-import shortid from 'shortid';
+import mqtt                    from 'mqtt';
+import {wr2,rd2,del2,forEach2} from '../util/hash2.js';
+import {emptyObj}              from '../util/emptyObj.js';
+import {afterPrefix}           from '../util/prefix.js';
+import shortid                 from 'shortid';
 
-// fnSpec={srcId,fName,pKey}
-// srv=(fnSpec,args,cb)=>done ... cb(data) ... done()
-export const mqttIpc=({ipcUrl,myId},srv)=>{
-  myId=myId || `client/${shortid.generate()}`;
+export const mqttIpc=(ipcSpec,lookup)=>{
+  if(!ipcSpec.myId) {
+    (ipcSpec={...ipcSpec, myId:`client/${shortid.generate()}`});
+  }
+  const {ipcUrl,myId}=ipcSpec;
 
   const online={};   // Which servers are active now?
   const mySubs={};   // My subscriptions with sub messages.
   const peerSubs={}; // Unsub handlers for peer subscriptions.
+  const cmds={};     // ipc commands
+  const defCmd=(cmd,handler)=>cmds[cmd]=handler;
 
   let qidCounter=0;  // Message channel id:
   const nextQid=()=>(++qidCounter).toString(36);
@@ -24,6 +28,8 @@ export const mqttIpc=({ipcUrl,myId},srv)=>{
   const client=mqtt.connect(ipcUrl,{will});
 
   const unsub=()=>{
+    forEach2(mySubs,  (channel)=>stopChannel(channel));
+    forEach2(peerSubs,(channel)=>stopChannel(channel));
     client.publish(okTopic,'',{retain,qos}); // like will
     client.end();
   };
@@ -40,75 +46,141 @@ export const mqttIpc=({ipcUrl,myId},srv)=>{
     client.publish(`data/${peerId}`,msg,{qos});
   } catch(e){ console.log('failed send:',peerId, msg); }};
 
-  const subscribe=(srcId,qid,msg,handler)=>{
-    wr2(mySubs,srcId,qid,{msg,handler});
-    if(online[srcId]) { send(srcId,msg); }
+  const subscribe=(peerId,qid,channel)=>{
+    wr2(mySubs,peerId,qid,channel);
+    if(online[peerId]) { startChannel(channel); }
   };
-  const unsubscribe=(srcId,qid)=>{
-    del2(mySubs,srcId,qid);
-  };
-
-  const peerSubscribe=(clientId,qid,cb)=>{
-    peerUnSubscribe(clientId,qid);
-    wr2(peerSubs,clientId,qid,cb);
-  };
-  const peerUnSubscribe=(clientId,qid)=>{
-    const cb=del2(peerSubs,clientId,qid);
-    if(cb) { cb(); }
+  const unsubscribe=(peerId,qid,done,confirmed)=>{
+    if(stopChannel(rd2(mySubs,peerId,qid),done,confirmed)) {
+      del2(mySubs,peerId,qid);
+    }
   };
 
-  // fnSpec={srcId,fName,pKey}
-  const subscribeFn=(fnSpec,args,cb)=>{
-    const {srcId}=fnSpec; const qid=nextQid();
-    const msg=['!','subscribe',[myId,qid,fnSpec,args]];
-    subscribe(srcId,qid,msg,cb);
-    return ()=>send(srcId,['!','unsubscribe',[myId,qid]]);
+  const peerSubscribe=(peerId,qid,channel)=>{
+    peerUnSubscribe(peerId,qid);
+    wr2(peerSubs,peerId,qid,channel);
+    startChannel(channel);
+  };
+  const peerUnSubscribe=(peerId,qid)=>{
+    const channel=del2(peerSubs,peerId,qid);
+    stopChannel(channel);
   };
 
-  subscribe('!','subscribe',null,([clientId,qid,fnSpec,args])=>{
-    const done=srv(fnSpec,args,(data)=>{
-      send(clientId,[myId,qid,data]);
-    },clientId);
-    peerSubscribe(clientId,qid,done);
+  const connectIpc=(cSpec,recv)=>{
+    const {peerId,fname,args}=cSpec;
+    const qid=nextQid();
+    let ok=true;
+    lookup(false,cSpec,({yomo,ctrl,fnSpec})=>{
+      const connect=(args2)=>
+        send(peerId,['subscribe',myId,qid,fname,args2]);
+      const sendData=(data)=>
+        send(peerId,['cData',myId,qid,data]);
+      const info={
+        yomo, fname,args, fnSpec,recv,
+        connect,sendData,
+        peerId, ipcSpec, client:true,
+      };
+      const channel={info,ctrl};
+      ok && subscribe(peerId,qid,channel);
+    });
+    return ()=>{
+      ok=false;
+      unsubscribe(peerId,qid,true,false);
+      send(peerId,['unsubscribe',myId,qid]);
+    };
+  };
+
+  const startChannel=(channel)=>{
+    // On the client:
+    // If the peer is now offline but gets online later,
+    // startChannel will be called automatically.
+    if(channel && online[channel.info.peerId]) {
+      const {ctrl:{start},info,active}=channel;
+      if(active) { stopChannel(channel); }
+      channel.active=true;
+      channel.state=start(info);
+      // ensure to call info.connect?(args) synchronously!
+    }
+  };
+
+  const procChannel=(channel,data)=>{ if(channel) {
+    const {active,ctrl:{proc},info,state}=channel;
+    if(active) { channel.state=proc(info,data,state); }
+  }};
+
+  const stopChannel=(channel,done,confirmed)=>{ if(channel) {
+    const {active,ctrl:{stop},info,state}=channel;
+    if(active) { stop(info,state); }
+    channel.state=channel.active=undefined;
+
+    if(done) { channel.done=true; }
+    if(channel.done && (confirmed || !online[info.peerId])) {
+      return true; // ok to delete the channel.
+    }
+    // a spurious confirmUnsub triggers a new startChannel:
+    if(confirmed && !channel.done) { startChannel(channel); }
+    return false; // don't delete the channel.
+  }};
+
+  defCmd('subscribe',([__,peerId,qid,fname,args])=>{
+    lookup(true,{peerId,fname,args},({yomo,ctrl,fnSpec})=>{
+      const sendData=(data)=>
+        send(peerId,['sData',myId,qid,data]);
+      const info={
+        yomo, fname,args, fnSpec, sendData,
+        peerId, ipcSpec, server:true
+      };
+      const channel={info,ctrl};
+      peerSubscribe(peerId,qid,channel);
+    });
   });
-  subscribe('!','unsubscribe',null,([clientId,qid])=>{
-    peerUnSubscribe(clientId,qid);
-    send(clientId,['!','confirmUnsub',[myId,qid]]);
+  defCmd('unsubscribe',([__,peerId,qid])=>{
+    peerUnSubscribe(peerId,qid);
+    send(peerId,['confirmUnsub',myId,qid]);
   });
-  subscribe('!','confirmUnsub',null,([srcId,qid])=>{
-    unsubscribe(srcId,qid);
+  defCmd('confirmUnsub',([__,peerId,qid])=>{
+    unsubscribe(peerId,qid,false,true);
   });
+  defCmd('sData',([__,peerId,qid,data])=>
+    const channel=rd2(mySubs,peerId,qid);
+    if(!channel) {console.log('no channel1 for:',peerId,qid);}
+    procChannel(channel,data);
+  );
+  defCmd('cData',([__,peerId,qid,data])=>
+    const channel=rd2(peerSubs,peerId,qid);
+    if(!channel) {console.log('no channel2 for:',peerId,qid);}
+    procChannel(channel,data);
+  );
 
-  client.on('message',(topic,data)=>{
-    //// console.log(`@${myId} ${topic}: ${data+''}`); ////
-    if(topic===myTopic) { try {
-      const [peerId,qid,args]=JSON.parse(data.toString());
-      const {handler}=(mySubs[peerId]||{})[qid] || {};
-      if(handler) { handler(args); }
-      else { console.log(
-        `unknown qid ${peerId}==>${myId}:`,qid,args
-      );}
+  client.on('message',(topic,msg)=>{
+    //// console.log(`@${myId} ${topic}: ${msg+''}`); ////
+    if(topic===dataTopic) { try {
+      msg=JSON.parse(msg.toString());
+      const handler=cmds[msg[0]];
+      if(handler) { handler(msg); }
+      else { console.log('unknown cmd:',msg); }
       return;
-    } catch(e) {return console.log('bad message',myId,data,e)}}
+    } catch(e) {return console.log('bad message',myId,msg,e);}}
 
     const peerId=afterPrefix('online/',topic);
     if(peerId){
       //// console.log('<== online:', peerId, data+''); ////
       if(data.toString()!=='') { // start-up: (re)subscribe
-        const x=mySubs[peerId]||{};
-        for(let qid in x){
-          const {msg}=x[qid];
-          msg && send(peerId,msg);
-        }
         online[peerId]=true;
+        const x=mySubs[peerId]||{};
+        for(let qid in x){ startChannel(x[qid]); }
       } else {                   // shut-down
-        const x=peerSubs[peerId]; delete peerSubs[peerId];
-        for(let qid in x){ x[qid](); }
         delete online[peerId];
+        const x=peerSubs[peerId]; delete peerSubs[peerId];
+        for(let qid in x){ stopChannel(x[qid]); }
+        const y=mySubs[peerId];
+        for(let qid in y){
+          if(stopChannel(y[qid],false,false)) { delete y[qid]; }
+        }
+        if(emptyObj(y)) { delete mySubs[peerId]; }
       }
-    } else { console.log('unknown topic:', topic, data+''); }
+    } else { console.log('unknown topic:', topic, msg+''); }
   });
 
-  return {myId, unsub, subscribeFn};
+  return {connectIpc,unsub};
 };
-
