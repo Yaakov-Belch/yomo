2a78641c8d6c90ee4acee85917f250910237016a
Yaakov Belch <github@yaakovnet.net>
2016-08-19 11:47:17 +0300

    ok

--- src/core/cacheFn.js
+++ src/core/cacheFn.js
@@ -10,10 +10,9 @@ export const metaFn=(fnTrafo)=>(...spec)=>{
   const fn=(yomo,...args)=> {
     const key=canon.stringify(args);
     const cache=yomo.cache[id]=yomo.cache[id] || {};
-    const data=cache[key]=cache[key] || xPeek({
-      cache, key, yomo, id, args,
-      res:fnTrafo(spec,yomo,args),
-    },yomo,id,args,true);
+    const data=cache[key]=cache[key] ||{
+      cache, key, res:fnTrafo(spec,yomo,args),
+    };
     return unwrapEx(cacheTrafo(data));
   };
   fn.id=id;
@@ -25,46 +24,13 @@ const cacheTrafo=createTransformer(
   (data)=>data.res.get(),
   (res,data)=>{
     delete data.cache[data.key];
-    xPeek(null,data.yomo,data.id,data.args,false);
     process.nextTick(()=>{
       if(!data.cache[data.key]) {
         if(data.res.unsub) { data.res.unsub(); }
-        if(data    .unsub) { data    .unsub(); } // obsolete?
       }
     })
   }
 );
-
-const xPeek=(res,yomo,id,args,add)=>{
-  let peeks;
-  if(yomo.peek && (peeks=yomo.peek[id])){ process.nextTick(()=>{
-    for(let k in peeks) { peeks[k](args,add); }
-  })};
-  return res;
-};
-
-export const yomoPeek=metaFn((spec,yomo,args)=>{
-  const id=nextId++;
-  const [fn,init,reducer,trafo]=spec;
-  const trafo2=trafo || (x=>x);
-  let state=init(...args);
-  const cc=yomo.cache[fn.id] || {};
-  for(let k in cc) {
-    const data=cc[k];
-    state=reducer(yomo,state,data.args,true);
-  }
-  const res=observable(asReference(trafo2(state)));
-  // use wr2?
-  if(!yomo.peek) { yomo.peek={}; }
-  yomo.peek[fn.id]=yomo.peek[fn.id]||{};
-  yomo.peek[fn.id][id]=mobx.action((args2,add)=>
-    res.set(trafo2(state=reducer(yomo,state,args2,add)))
-  );
-  res.unsub=()=>{
-    delete yomo.peek[fn.id][id]; // use del2?
-  };
-  return res;
-});
 
 export const cacheFn=metaFn(([fn],yomo,args)=>{
   return {get:()=>{
--- src/experimental.js
+++ src/experimental.js
@@ -1,4 +1,4 @@
-import {cacheSlow,yomoPeek} from './core/cacheFn.js';
+import {cacheSlow,cacheFnu} from './core/cacheFn.js';
 import {reuse,combineReducers} from './util/reuse.js';
 import {linkPipes,pipes,getPipe} from './core/pipes.js';
 import {getBuffer,getText,getJSON} from './util/getx.js';
@@ -13,11 +13,10 @@ export {
   yomoBridge,              // try and approve
   linkPipes,pipes,getPipe, // review, approve or sep package
   cacheSlow,               // review API, improve implementation
+  cacheFnu,                // keep internal
 
   reuse,combineReducers,     // release as separate package
   getBuffer,getText,getJSON, // release as separate package
   delay,ticks,               // release as separate package
-
-  yomoPeek,                // remove API
-}; 
+};
 
