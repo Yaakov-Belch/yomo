0060c3a19d2685b9d11cdddba47a85bf27410820
Yaakov Belch <github@yaakovnet.net>
2016-09-13 16:57:19 +0300

    ok

---
+++ src/util/xFs.js
@@ -0,0 +1,37 @@
+import {watch,readFile,readdir} from 'fs';
+import {cacheCb} from '../core/cacheFn.js';
+
+export const xRdJson=(yomo,path)=>JSON.parse(xRdTxt(yomo,path));
+export const xRdTxt=(yomo,path)=>xRdBuffer(yomo,path).toString();
+export const xRdBuffer=cacheCb((yomo,cb,path)=>{
+  let old;
+  const look=()=>readFile(path,(error,res)=>{
+    if((!old) || res.compare(old)){
+      cb(error,old=res);
+    }
+  });
+  const watcher=watch(path,look);
+  look();
+  return ()=>watcher.close();
+});
+
+export const xRdDir=cacheCb((yomo,cb,path)=>{
+  let old;
+  const look=()=>readdir(path,(error,res)=>{
+    res.sort();
+    if(!(old && arraysEqual(res,old))){
+      cb(error,old=res);
+    }
+  });
+  const watcher=watch(path,look);
+  look();
+  return ()=>watcher.close();
+});
+
+const arraysEqual=(a,b)=>{
+  if(a.length != b.length) { return false; }
+  for(let i=0; i<a.length; i++) {
+    if(a[i]!==b[i]){ return false; }
+  }
+  return true;
+}
