f509be15ca27009ceb41373f98802182e6d5d7d1
Yaakov Belch <github@yaakovnet.net>
2016-09-14 15:11:40 +0300

    ok

--- src/core/component.js
+++ src/core/component.js
@@ -1,7 +1,9 @@
 import {indexFast} from './indexKey.js';
 
 export const yomoSelector=(key)=>(yomo)=>yomo.yomoState(key).get();
-export const yomoDispatcher=(key,reducer)=>indexFast((yomo,action)=>{
-  const r=yomo.yomoState(key);
-  r.write(reducer(r.data,action));
-});
+export const yomoDispatcher=(key,reducer)=>indexFast((yomo,action)=>
+  process.nextTick(()=>{
+    const r=yomo.yomoState(key);
+    r.write(reducer(r.data,action));
+  })
+);
